2c02385d30f0459e296e0e02d979eac72f73c6d5
Arunkumar Krishnamoorthy <arunram.krish@gmail.com>
2015-12-02 15:07:12 +0530

    plain node js mvc

--- labs_nov/1_Node/app.js
+++ labs_nov/1_Node/app.js
@@ -14,3 +14,9 @@ console.log("User Details" + user1.display());
 
 var user2 = userFactory("Ram", "12-12-12", "9449804064");
 console.log("User Details" + user2.display());
+
+var userDao = require("./dao/userDao.js");
+userDao.create(user1);
+userDao.create(user2);
+
+console.log("from DAO " + userDao.readByName("Arun").display());
---
+++ labs_nov/1_Node/dao/userDao.js
@@ -0,0 +1,3 @@
+var userDao = require("./file/userDao.js");
+
+module.exports = userDao;
