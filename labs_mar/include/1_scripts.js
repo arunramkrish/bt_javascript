acce56eb4d4f450fa2fd6729fbe5cd06fb5251bc
Arunkumar Krishnamoorthy <arunram.krish@gmail.com>
2016-03-08 16:06:37 +0530

    day 1 labs

---
+++ labs_mar/include/1_scripts.js
@@ -0,0 +1,4 @@
+console.log("printing from js file " + ((getMessage()) ? (getMessage()) : " My default message"));
+function getMessage() {
+	"hello JS from included function";
+}
---
+++ labs_mar/include/6_datatable.js
@@ -0,0 +1,23 @@
+/*
+Populates HTML element with id as containerId with a HTML table. Table has columns from properties present in the first element of tableData. Each record in tableData should correspond to row
+
+containerId : String id of the div or html container
+TableData : Array of javascript objects
+*/
+function Datatable(containerId, tableData) {
+    
+    //get the container element
+    
+    //create html table element
+    
+    //use HTMLTableElement API to insert row / header
+    
+    //iterate thro prop names in the first record of table data and create columns
+    
+    //for each record in tableData insert row in table element 
+    
+    // for each field in the current record, populate cell text
+    
+    //append table element to container
+    
+}
